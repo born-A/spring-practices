caeb54e161aef6ff4ce43fe30e6875719c77faeb
born-A <93516595+born-A@users.noreply.github.com>
2024-06-10 14:51:32 +0900

    spring aop

--- guestbook3/src/main/java/com/poscodx/guestbook/controller/GuestbookController.java
+++ guestbook3/src/main/java/com/poscodx/guestbook/controller/GuestbookController.java
@@ -10,22 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.poscodx.guestbook.repository.GuestbookRepositoryWithJdbcContext;
-import com.poscodx.guestbook.repository.GuestbookRepositoryWithJdbcTemplate;
+import com.poscodx.guestbook.service.GuestbookService;
 import com.poscodx.guestbook.vo.GuestbookVo;
 
 @Controller
 public class GuestbookController {
 	@Autowired
-	private GuestbookRepositoryWithJdbcContext guestbookRepository2;
-	
-	@Autowired
-	private GuestbookRepositoryWithJdbcTemplate guestbookRepository3;
-	
+	private GuestbookService guestbookService;
 	
 	@RequestMapping("/")
 	public String index(Model model) {
-		List<GuestbookVo> list = guestbookRepository2.findAll();
+		List<GuestbookVo> list = guestbookService.getContentsList();
 		model.addAttribute("list", list);
 		
 		return "index";
@@ -33,7 +28,7 @@ public class GuestbookController {
 
 	@RequestMapping("/add")
 	public String add(GuestbookVo vo) {
-		guestbookRepository2.insert(vo);
+		guestbookService.addContents(vo);
 		return "redirect:/";
 	}
 
@@ -45,7 +40,7 @@ public class GuestbookController {
 	
 	@RequestMapping(value="/delete/{no}", method=RequestMethod.POST)
 	public String delete(@PathVariable("no") Long no, @RequestParam(value="password", required=true, defaultValue="") String password) {
-		guestbookRepository2.deleteByNoAndPassword(no, password);
+		guestbookService.deleteContents(no, password);
 		return "redirect:/";
 	}
 }
